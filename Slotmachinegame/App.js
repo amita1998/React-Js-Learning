af4afa1b7a3a1960bd22fa467682c6303324c2aa
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-06-15 18:54:24 +0530

    Add files via upload

--- Slotmachinegame/App.js
+++ Slotmachinegame/App.js
@@ -1,1 +1,69 @@
+import React from "react";
 
+
+const SlotM = (props) => {
+
+    // let x = "😄";
+    // let y = "😄";
+    // let z = "🎅";
+
+    let x = props.x;
+    let y = props.y;
+    let z = props.z;
+
+
+if (x===y  && y===z)
+{
+  return(
+      <>
+      <div className="slot_inner">
+
+          <h1> 
+              {x} {y} {z}
+            </h1>
+          <h1> This is matching</h1>
+          <hr/>
+      </div>
+      </>
+  );
+  }else{
+
+    return(
+        <>
+        <div className="slot_inner">
+  
+            <h1> 
+                {x} {y} {z}
+              </h1>
+            <h1> This is not matching</h1>
+            <hr/>
+        </div>
+        </>
+    );
+
+
+};
+}
+
+const App = () => {
+
+    return (
+    <>
+    <h1 className="heading_style">
+       🎰 Welcome to   <span style={{fontWeight: "bold"}}>Slot machine game 🎰</span>
+    </h1>
+   <div className="slotmachine">
+   <SlotM  x ="😄"  y="😄"  z="😄"/>
+   <hr />
+   <SlotM   x ="😄"  y="😈"  z="🎅"/>
+   <hr />
+   <SlotM   x ="💑"  y="💑"  z="💑" />
+   <hr />
+   <SlotM   x ="😽"  y="🐱"  z="😹" />
+   <hr />
+   </div>  
+    </>
+ );
+};
+
+export default App;
