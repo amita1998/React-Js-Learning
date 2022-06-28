25854252981845dd4568e211e47e6d9dd74b801f
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-06-28 13:40:13 +0530

    Add files via upload

--- CRUD/App.js
+++ CRUD/App.js
@@ -1,1 +1,26 @@
+import React from "react";
+import "./App.css"; 
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import NavbarComponent from "./components/layout/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddUser from "./components/users/AddUser";
 
+const App = () => {
+  return (
+    <div className="app">
+      <Router>
+      <NavbarComponent/>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users/add" element={<AddUser />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
