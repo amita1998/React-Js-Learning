dd8c90d887e96461e643aa501b2ad3a41692e00c
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-07-01 22:09:26 +0530

    Add files via upload

--- React Forms/App.js
+++ React Forms/App.js
@@ -1,1 +1,35 @@
+import React, { useState } from "react";
 
+
+const App = () => 
+{
+
+  const [name , setName] = useState();
+  const [fullName , setfullName] = useState();
+
+
+  const inputEvent = (event) => {
+
+      // console.log(event.target.value);
+      setName(event.target.value);
+  };
+
+  const onSubmit = () => {
+    setfullName(name);
+  }
+  return(
+    <>
+      <div>
+        <h1>Hello {fullName}</h1>
+        <input type="text" placeholder="Enter Your Name" onChange={inputEvent} 
+          value={name}
+        />
+        <button onClick={onSubmit}>Click me </button>
+
+      </div>
+    </>
+  );
+
+};
+
+export default App;
