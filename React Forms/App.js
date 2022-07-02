e29252125088d69da30756f1b819d04aec5040e8
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-07-02 18:20:40 +0530

    Update App.js

--- React Forms/App.js
+++ React Forms/App.js
@@ -5,7 +5,11 @@ const App = () =>
 {
 
   const [name , setName] = useState();
+
+  const [lastname , setLastName] = useState();
+
   const [fullName , setfullName] = useState();
+
 
 
   const inputEvent = (event) => {
@@ -14,18 +18,37 @@ const App = () =>
       setName(event.target.value);
   };
 
-  const onSubmit = () => {
+  const inputEventTwo = (event) => {
+
+      setLastName(event.target.value);
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
     setfullName(name);
   }
   return(
     <>
+      <div className="main_div">
+      <form onSubmit={onSubmit}>
       <div>
         <h1>Hello {fullName}</h1>
-        <input type="text" placeholder="Enter Your Name" onChange={inputEvent} 
-          value={name}
-        />
-        <button onClick={onSubmit}>Click me </button>
+        <input type="text" placeholder="Enter Your Last Name" 
+        onChange={inputEvent} 
+          value={name} /> 
+          <br />
 
+          <input type="text" placeholder="Enter Your Name" 
+        onChange={inputEventTwo} 
+          value={lastName} /> 
+         
+
+          
+       
+        <button type="submit">Click me </button>
+
+      </div>
+      </form>
       </div>
     </>
   );
