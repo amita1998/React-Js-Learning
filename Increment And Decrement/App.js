d2eae21f2590bc25ecf06d8a0c548e366a7792b1
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-07-07 19:13:38 +0530

    Add files via upload

--- Increment And Decrement/App.js
+++ Increment And Decrement/App.js
@@ -1,1 +1,42 @@
+import React from "react";
+import { useState } from "react";
 
+const App = () =>{
+
+const [num,setnum] = useState(0);
+
+const incNum = () => {
+
+  setnum(num + 1);
+};
+
+const decNum = () =>{
+  if(num > 0)
+  {
+  setnum(num -1);
+  }
+  else{
+    alert("Sorry,Zero Limit Reached");
+      setnum(0);
+  }
+};
+
+
+  return(
+    <>
+      <div className="main_div">
+        <div className="center_div">
+
+          <h1> {num} </h1>
+          <div className="btn_div">
+
+          <button onClick={incNum}>Increment</button>
+          <button onClick={decNum}>Decrement</button>
+          </div>
+        </div>
+      </div>
+    </>
+
+  );
+};
+export default App;
