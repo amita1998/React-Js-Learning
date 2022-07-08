e7556cff1915b7e98f79bf2dfbb3a363b3de8b9a
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-07-08 19:48:30 +0530

    Add files via upload

--- ToDo List/App.js
+++ ToDo List/App.js
@@ -1,1 +1,41 @@
+import React from "react";
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import { useState } from "react";
 
+const App = () => {
+
+  const [item,setItem] = useState();
+
+  const itemEvent = (event) => {
+    setItem(event.target.value);
+
+  }
+
+  return(
+    <>
+      <div className="main_div">
+        <div className="center_div">
+        <br/>
+        <h1>ToDo List</h1>
+        <br/>
+
+        <input  type="text" placeholder="Add An Items" onChange={itemEvent}/>
+        <Button className="newbtn">
+          <AddIcon/>
+          </Button>
+          <br/>
+
+          <ol>
+            <li>{item}</li>
+          </ol>
+          <br/>
+
+        </div>
+
+      </div>
+    </>
+  );
+};
+
+export default App;
