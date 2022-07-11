769e422d32de2ff04d0be20e9d923e0a08e0a988
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-07-11 22:53:42 +0530

    Update App.js

--- ToDo List/App.js
+++ ToDo List/App.js
@@ -3,14 +3,24 @@ import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import { useState } from "react";
 
+
 const App = () => {
 
   const [item,setItem] = useState();
+  const [newitem,setNewItem] = useState([]);
 
   const itemEvent = (event) => {
     setItem(event.target.value);
 
   }
+
+  const ListOfItems = () => {
+    setNewItem((prevValue)=>{
+      return [...prevValue, item]
+    });
+
+      setItem(" ");
+  };
 
   return(
     <>
@@ -20,14 +30,17 @@ const App = () => {
         <h1>ToDo List</h1>
         <br/>
 
-        <input  type="text" placeholder="Add An Items" onChange={itemEvent}/>
-        <Button className="newbtn">
+        <input  type="text" value={item} placeholder="Add An Items" onChange={itemEvent}/>
+        <Button className="new_btn" onClick={ListOfItems}>
           <AddIcon/>
           </Button>
           <br/>
 
           <ol>
-            <li>{item}</li>
+          
+            {newitem.map((val) => {
+              return  <li>{val}</li>;
+            })}
           </ol>
           <br/>
 
