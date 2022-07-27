2a2f99baef97df54d3e129780fd8bf4abce9b9bb
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-07-27 23:19:06 +0530

    Add files via upload

---
+++ Gallery Web/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Menu from './menu';
+
+
+ReactDOM.render(<>
+  <App/>
+  <Menu/>
+</>,document.getElementById("root"));
