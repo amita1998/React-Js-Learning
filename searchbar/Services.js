505b732c40f8bf7147a5d8c8e5bdd909645c574c
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-08-09 20:26:33 +0530

    Update Services.js

--- searchbar/Services.js
+++ searchbar/Services.js
@@ -1,16 +1,13 @@
 import React from "react";
-import './index.css';
-import { Route, Link } from "react-router-dom";
 
 const Service = () =>{
 
- 
-        return (
-          <div className="App">
-      <h1> Hi i am Services page</h1>
-          </div>
-        );
+    return(
+        <>
+            <h1>Hello , This is Service Page</h1>
+        </>
 
+    );
 };
 
 export default Service;
