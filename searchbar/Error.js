8d8b3eb60eb115b6e87f8e8983b274b27297eb0d
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-08-09 20:24:49 +0530

    Update Error.js

--- searchbar/Error.js
+++ searchbar/Error.js
@@ -1,16 +1,11 @@
 import React from "react";
-import './index.css';
-import {NavLink} from 'react-router-dom';
 
-const Error = () => {
+const Error = () =>{
 
     return(
-        <div className="App">
-        <h1>404 Error Page</h1>
-        <p>Sorry,this pae is not available</p>
-        <NavLink to="/">Go Back</NavLink>
-
-        </div>
+        <>
+            <h1>OOPS!!  Page Not Found!</h1>
+        </>
 
     );
 };
