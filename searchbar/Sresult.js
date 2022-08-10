1b2be399515d98adc4a0089569d606fd265aac45
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-08-10 23:54:37 +0530

    Update Sresult.js

--- searchbar/Sresult.js
+++ searchbar/Sresult.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-const Sresult = () => {
+const Sresult = (props) => {
 
-    const img = 'https://source.unsplash.com/300x300?mango';
+    const img = `https://images.unsplash.com/300x300/?${props.name}`;
+    // const img = `https://images.unsplash.com/photo-1570913149827-d2ac84ab3f9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80/300x300/?${props.name}`;
 
     return(
         <>
