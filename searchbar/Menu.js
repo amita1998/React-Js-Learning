ccf6788665f65c013e8961b883adf5e4c72504f2
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-08-10 23:56:03 +0530

    Update Menu.js

--- searchbar/Menu.js
+++ searchbar/Menu.js
@@ -11,6 +11,7 @@ const Menu = () => {
         <div className="menu_style">
             <NavLink activeClassName="active_class" to="/"> About Us </NavLink>
             <NavLink activeClassName="active_class"  to="/service"> Service</NavLink>
+            <NavLink activeClassName="active_class"  to="/search"> Search</NavLink>
             <NavLink activeClassName="active_class"  to="/user/Mini & Micky"> User</NavLink>
             <NavLink activeClassName="active_class"  to="/contact"> Contact Us</NavLink>
 
