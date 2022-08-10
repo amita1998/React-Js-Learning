81489aa2e2487bbdf0b00daefdfdbc2e55bba606
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-08-10 14:09:29 +0530

    Update Search.js

--- searchbar/Search.js
+++ searchbar/Search.js
@@ -1,23 +1,27 @@
-import React, { useState } from 'react';
-import './index.css';
-import Sresult from './Sresult';
+import React, { useState } from "react";
+import Sresult from "./Sresult";
 
-const Search = () => {
+const Search = () =>{
 
-const[img, setImg] = useState('');
- const InputEvent=(event)=>
- {
-   setImg(event.target.value);
- }
+    const [img,setImg] = useState(""); 
 
- return (
-    <div className="searchbar">
-<input  type='text' placeholder='Search anything' value={img} onChange={InputEvent} />
-{img=== "" ? null :<Sresult name={img} />   }
-    </div>
-  );
-}
+    const inputEvent = (event) => {
+
+        const data = event.target.value;
+        console.log(data);
+        setImg(data);
+    }
+
+    return(
+        <>
+        <div className="serachbar">
+        <input type="text" placeholder="Serach Anything?" value={img} onChange={inputEvent} />
+        <Sresult/>
+        </div>
+        </>
+    );
 
 
+};
 
 export default Search;
