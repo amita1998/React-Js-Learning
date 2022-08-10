32299ba848dec7bb6335802bce99f3356179bbcc
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-08-10 14:10:05 +0530

    Update User.js

--- searchbar/User.js
+++ searchbar/User.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import './index.css';
-import { useLocation, useParams } from "react-router-dom";
+import React from "react";
+import { useParams , useLocation } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
-const USer = () => 
-{
+// const User = ({match}) =>{
+//     return <h1>user{match.params.fname}</h1>;
+// };
 
-    const {name}  =useParams();
-    const location=useLocation();
+const User = () => {
 
-    return (
-        <div className="App">
-    <h1> Hi i am User {name} page</h1>
-    my current location is {location.pathname}
-    <br/>
-    {location.pathname===`/user/pari`? (<button>Hi</button>): null }
-        </div>
-      );
-    
+    const {fname} = useParams();
+    const location = useLocation();
+    const navigate = useNavigate();
+    console.log(location);
+    console.log(navigate);
 
-}
+    return(
+        <>
+    <h1>User {fname} Page</h1>
+    <br></br>
+    <h4>My Current Location is {location.pathname}</h4>
+    {location.pathname === '/user/Mini%20&%20Micky'? (
+        <button onClick={() => navigate.push("/about")}>Home Page</button>
+    ) : null}
+    </>
+    );
+
+};
+
 export default User;
