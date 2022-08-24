971d58b44f758cdcf1e2539de4eee6aa2c7a92b0
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-08-24 22:43:13 +0530

    Add files via upload

--- TextEditor/components/LoginWithLocalStorage/Home.js
+++ TextEditor/components/LoginWithLocalStorage/Home.js
@@ -1,1 +1,17 @@
+import React from "react";
 
+const Home = () => {
+
+    const handleClick=()=>{
+        localStorage.clear();
+        window.location.reload();
+    }
+    return (
+        <>
+            <h1>Home Page</h1>
+            <button onClick={handleClick}>Logout</button>
+        </>
+    )
+
+}
+export default Home;
