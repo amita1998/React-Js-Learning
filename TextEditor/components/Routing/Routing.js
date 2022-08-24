287dbadf7265bb798c8ab749e43c321405aa8a31
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-08-24 22:41:20 +0530

    Add files via upload

--- TextEditor/components/Routing/Routing.js
+++ TextEditor/components/Routing/Routing.js
@@ -1,1 +1,22 @@
+import React from "react";
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import Form from "../FormValidation/Form";
 
+
+function RoutingReact() {
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Form/>}/>
+                    
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+export default RoutingReact;
