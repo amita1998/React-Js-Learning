2824f8b2769f2c268aa6ae32f8b3007897f2e6d1
Patel Amita <68098511+amita1998@users.noreply.github.com>
2022-09-03 23:04:36 +0530

    Update Routing.js

--- TextEditor/components/Routing/Routing.js
+++ TextEditor/components/Routing/Routing.js
@@ -1,11 +1,8 @@
 import React from "react";
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from "react-router-dom";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import GetApiCall from "../FetchApiCall/GetApiCall";
 import Form from "../FormValidation/Form";
-
+import Loading from "../Loading/Loading";
 
 function RoutingReact() {
     return (
@@ -13,7 +10,8 @@ function RoutingReact() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Form/>}/>
-                    
+                    <Route path="/load" element={<Loading/>}/>
+                    <Route path="/api" element={<GetApiCall/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
