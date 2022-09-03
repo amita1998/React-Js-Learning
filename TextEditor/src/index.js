bfc1e6ca87a1a2e89cd3cf94bc9738493e24ceff
DESKTOP-HCSN70J\HP <amita.patel717@gmail.com>
2022-09-03 23:49:38 +0530

    Minor bug fixing

---
+++ TextEditor/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Htmleditor from "./components/HtmlEditor/HtmlEditor";
+import AddDynamicInput from "./components/AddDynamicInput/AddDynamicInput";
+import LocalStorage from "./components/LocalStorage/LocalStorage";
+
+
+ReactDOM.render (
+  <App/>,document.getElementById("root")
+);
+
+
