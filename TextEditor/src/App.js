09a49ce5c3ee1ee841c46b8f67cf9563e48b109a
DESKTOP-HCSN70J\HP <amita.patel717@gmail.com>
2022-09-22 06:04:01 +0530

    Code Refactoring

--- TextEditor/src/App.js
+++ TextEditor/src/App.js
@@ -1,31 +1,17 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
+import RichTextEditor from './components/RichTextEditor';
+
 function App() {
-  
-  const words=[
-    {id:0,value:"Apple"},
-    {id:1,value:"Mango"},
-    {id:2,value:"Banana"},
-    {id:3,value:"Banana1"},
-    {id:4,value:"Banana2"},
-  ];
-  const [wordData,setWordData]=useState(words[0].value)
-  const handleClick=(index)=>{
-    console.log(index)
-    const wordSlider=words[index].value;
-    setWordData(wordSlider)
-  }
   return (
+    
     <div className="App">
-      <div>" {wordData} "</div>
-      <div className='flex_row'>
-        {words.map((data,i)=>
-          <h1 key={i} onClick={()=>handleClick(i)}>.</h1>
-        )}
-      </div>
+    <h1>Rich Text Editor</h1>
+
+    <RichTextEditor/>
+     
     </div>
   );
 }
-
 
 export default App;
